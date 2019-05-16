9c5a4585f82386c061c59c5b7a1bc933d9b2852f
AKuznecovs <andrejs_kuznecovs@inbox.lv>
2019-05-16 23:53:30 +0300

    update

---
+++ script/script.js
@@ -0,0 +1,45 @@
+window.onscroll = function() {
+    stickyBar();
+    scrollFunction();
+    scrollFunctionTwo();
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyBar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navbar.classList.add("sticky2");
+    navbar.appendChild
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky2");
+  }
+};
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function scrollFunctionTwo() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.querySelector(".boxOl").style.display = "block";
+  } else {
+    document.querySelector(".boxOl").style.display = "none";
+  }
+}
+
+
+
